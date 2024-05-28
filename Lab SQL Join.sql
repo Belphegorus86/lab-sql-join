4d60351d19ed4868a4ceee0e814c2d2527e049a5
Belphegorus86 <156254077+Belphegorus86@users.noreply.github.com>
2024-05-28 02:50:49 +0100

    Add files via upload

---
+++ Lab SQL Join.sql
@@ -0,0 +1,41 @@
+USE sakila;
+
+
+# 1 -->
+
+SELECT c.name AS Category, COUNT(fc.film_id) AS `Number of Films`
+FROM film_category AS fc
+JOIN category AS c ON fc.category_id = c.category_id
+GROUP BY c.name
+ORDER BY `Number of Films` DESC;
+
+# 2 -->
+
+SELECT s.first_name, s.last_name, a.address
+FROM staff AS s
+JOIN address AS a ON s.address_id = a.address_id;
+
+# 3 -->
+
+SELECT s.staff_id, s.first_name, s.last_name, SUM(p.amount) AS total_amount
+FROM payment p
+JOIN staff s ON p.staff_id = s.staff_id
+WHERE p.payment_date BETWEEN '2005-08-01' AND '2005-08-31'
+GROUP BY s.staff_id, s.first_name, s.last_name
+ORDER BY total_amount DESC;
+
+# 4 -->
+
+SELECT f.title, COUNT(fa.actor_id) AS `Number of Actors`
+FROM film_actor fa
+JOIN film f ON fa.film_id = f.film_id
+GROUP BY f.title
+ORDER BY `Number of Actors` DESC, f.title;
+
+# 5 -->
+
+SELECT c.last_name, c.first_name, SUM(p.amount) AS `Total Amount Paid`
+FROM payment p
+JOIN customer c ON p.customer_id = c.customer_id
+GROUP BY c.customer_id
+ORDER BY c.last_name, c.first_name;
